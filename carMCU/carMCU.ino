206970483f5522e4dffaa02429f407aa45ddc9ad
FPast <chgy2131@naver.com>
2021-01-04 22:49:00 +0900

    sadf

--- carMCU/carMCU.ino
+++ carMCU/carMCU.ino
@@ -1,19 +1,19 @@
 ///////////////////////////////////////
 //
 // BlueTooth miniCar
-// ver alpha , 2021-01-04
+// ver 1.00 , 2021-01-04
 //
 ///////////////////////////////////////
 #include <SoftwareSerial.h>
 #define DATASIZE 4
 
-const int LED_FRONT = 1;
-const int LED_BACK = 2;
+const int LED_FRONT = 12;
+const int LED_BACK = 13;
 const int BLUE_RX = 3;
 const int BLUE_TX = 4;
-const int MOTOR_DCF = 5;
-const int MOTOR_DCB = 6;
-const int MOTOR_SER = 7;
+const int MOTOR_DCF = 6;
+const int MOTOR_DCB = 5;
+const int MOTOR_SER = 10;
 
 int reciveData[DATASIZE] = {0, 0, 127, 0}; // DC Motor F, DC Motor B, Servo, function
 void writeErrorMsg(char*);
@@ -33,21 +33,32 @@ void setup() {
 
 void loop() {
     while(true) {
-        //UART Check
-        if(BlueSerial.available() == DATASIZE) {
-            for(int i=0; i<DATASIZE; i++) {
-                reciveData[i] = BlueSerial.read();
-                
+        //UART recive & check
+        if(BlueSerial.available()) {
+            if(BlueSerial.read() == 'S') {
+              for(int i=0; i<DATASIZE; i++) {
+                  reciveData[i] = BlueSerial.read();
+              }
+              if(BlueSerial.read() != 'E') {
+                writeErrorMsg("[error] End sign is not send.");
+                continue;
+              }
+              else if( (reciveData[3] & 0b00000110) != 0b00000010) {
+                writeErrorMsg("[error] check bit is wrong... 0&1");
+                continue;
+              }
+              else if( ((reciveData[0] & 1) | (reciveData[1] & 1) | (reciveData[2] & 1)) & 1 != reciveData[3] & 1 ) {
+                writeErrorMsg("[error] check bit is wrong... P");
+                continue;
+              }
+            }
+            else {
+              continue;
             }
         } else {
-            if(BlueSerial.available()) {
-                writeErrorMsg("[warning] Bluetooth module sended strange value ... so arduino delete that");
-            }
-            while(BlueSerial.available())
-                BlueSerial.read();
             continue;
         }
-    
+   
         //Servo Motor
         analogWrite(MOTOR_SER, reciveData[2]); //0~127 LEFT , 128~256 RIGHT
         
